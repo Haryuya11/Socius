7be3b5ed226cf105b5c39fbb0637fc2d3d848967
giakhang0812 <phamnguyengiakhang14@gmail.com>
2025-12-14 13:09:00 +0700

    feat: add constants for department and role management

--- socius-core-modules/src/main/java/com/uit/sociuscoremodules/department/constants/DepartmentConstant.java
+++ socius-core-modules/src/main/java/com/uit/sociuscoremodules/department/constants/DepartmentConstant.java
@@ -4,14 +4,19 @@ package com.uit.sociuscoremodules.department.constants;
 public final class DepartmentConstant {
   /** Private constructor to prevent instantiation. */
   private DepartmentConstant() {}
+
   /** Key for department code. */
   public static final String DEPARTMENT_CODE = "departmentCode";
+
   /** Key for employee ID. */
   public static final String EMPLOYEE_ID = "employeeId";
+
   /** Key for role code. */
   public static final String TO_DEPARTMENT_CODE = "toDepartmentCode";
+
   /** Key for role code. */
   public static final String FROM_DEPARTMENT_CODE = "fromDepartmentCode";
+
   /** Key for role code. */
   public static final String ROLE_CODE = "roleCode";
 }
