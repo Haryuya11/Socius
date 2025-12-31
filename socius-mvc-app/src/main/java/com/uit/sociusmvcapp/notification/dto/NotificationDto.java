aff0fdd65905b3902e70d461cda621c0ff2ba288
Võ Chính Hiệu <vochinhhieu11082004@gmail.com>
2025-12-31 23:03:38 +0700

    Feature/update rabbitmq config (#33)

--- socius-mvc-app/src/main/java/com/uit/sociusmvcapp/notification/dto/NotificationDto.java
+++ socius-mvc-app/src/main/java/com/uit/sociusmvcapp/notification/dto/NotificationDto.java
@@ -1,6 +1,8 @@
 package com.uit.sociusmvcapp.notification.dto;
 
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,5 +21,7 @@ public class NotificationDto {
   private Short deliveryType;
   private PayloadDto payload;
   private Short isRead;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
   private LocalDateTime createdAt;
 }
